7ed08e0475087db87df52c7626c2ea9dfcee6d45
Chad <wehbe.chady@gmail.com>
2022-02-06 19:43:40 +0100

    CAGE-5
    Refactoring: remove the usage of "noop" classes.

--- tests/unit/AveragePostsPerUserTest.php
+++ tests/unit/AveragePostsPerUserTest.php
@@ -3,12 +3,12 @@
 namespace Tests\unit;
 
 use PHPUnit\Framework\TestCase;
-use Statistics\Calculator\NoopCalculator;
+use Statistics\Calculator\AveragePostsPerUser;
 use Statistics\Dto\ParamsTo;
 use Statistics\Dto\StatisticsTo;
 use Statistics\Enum\StatsEnum;
 
-class NoopCalculatorTest extends TestCase
+class AveragePostsPerUserTest extends TestCase
 {
     protected ParamsTo $paramsTo;
 
@@ -19,9 +19,9 @@ class NoopCalculatorTest extends TestCase
 
     public function testDoCalculate()
     {
-        $noopCalculator = new NoopCalculator();
+        $averagePostsPerUser = new AveragePostsPerUser();
 
-        $noopCalculator->totals = [
+        $averagePostsPerUser->totals = [
             0 => ['userName' => 'testUser1', 'postCount' => 3],
             1 => ['userName' => 'testUser2', 'postCount' => 2],
             2 => ['userName' => 'testUser3', 'postCount' => 32],
@@ -29,10 +29,10 @@ class NoopCalculatorTest extends TestCase
 
         $this->paramsTo->method('getStatName')->willReturn(StatsEnum::AVERAGE_POST_NUMBER_PER_USER);
 
-        $noopCalculatorCalculate = $noopCalculator->doCalculate();
+        $averagePostsPerUserCalculate = $averagePostsPerUser->doCalculate();
 
-        $this->assertInstanceOf(StatisticsTo::class, $noopCalculatorCalculate);
-        $this->assertCount(3, $noopCalculatorCalculate->getChildren());
-        $this->assertInstanceOf(StatisticsTo::class, $noopCalculatorCalculate->getChildren()[0]);
+        $this->assertInstanceOf(StatisticsTo::class, $averagePostsPerUserCalculate);
+        $this->assertCount(3, $averagePostsPerUserCalculate->getChildren());
+        $this->assertInstanceOf(StatisticsTo::class, $averagePostsPerUserCalculate->getChildren()[0]);
     }
 }
